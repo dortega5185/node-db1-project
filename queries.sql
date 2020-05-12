9e21e42562729e9d21d6947a9a3fdeabbfc75313
David Mizativan-Ortega <dortega5185@gmail.com>
2020-05-11 21:44:58 -0500

    done with mvp

--- queries.sql
+++ queries.sql
@@ -23,10 +23,26 @@ limit 10
 
 -- Find all customers that live in London, Madrid, or Brazil
 
+select * from customers where city = 'London' or city = 'Madrid' or country = 'Brazil'
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+
+insert into customers (customerName, contactName, address, city, postalCode, country)
+values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
+update customers
+set postalCode = '11122'
+where customerId = 92
+
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+select distinct city
+from customers
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+select *
+from suppliers
+where length(supplierName) > 20
